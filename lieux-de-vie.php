c822fc44ad97f6c7634c96900d77f6a06238f62a
Jeremy <wujeremy96@gmail.com>
2024-12-19 11:41:29 +0100

    Ajouter fichier lieux de vie

---
+++ lieux-de-vie.php
@@ -0,0 +1,47 @@
+<?php
+    $couleur_bulle_classe = "noir";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LIEUX DE VIE | BUT MMI</title>
+
+    <link rel="stylesheet" href="ressources/css/ne-pas-modifier/reset.css">
+    <link rel="stylesheet" href="ressources/css/ne-pas-modifier/fonts.css">
+    <link rel="stylesheet" href="ressources/css/ne-pas-modifier/global.css">
+    <link rel="stylesheet" href="ressources/css/ne-pas-modifier/header.css">
+</head>
+
+<body>
+    <section class="conteneur-1280">
+        <?php require_once('./ressources/includes/header.php'); ?>
+
+        <!-- Vous allez principalement écrire votre code HTML ci-dessous -->
+        <main class="conteneur-principal">
+            <h1 class="titre-page">Lieux de vie</h1>
+
+            <section class="colonne">
+                <section class="liste-articles">
+                    <p>METTEZ VOS ARTICLES A L'INTERIEUR DE LA BALISE ".liste-articles" tout en supprimant cette balise &lt;p></p>
+                </section>
+                <a class="jpo-banniere" title="Ouverture dans un nouvel onglet" href="https://www.cyu.fr/salons-journee-portes-ouvertes">
+                    <img src="ressources/images/logo-cyu-blanc.png" width="200" class="logo" alt="">
+                    
+                    <section class="textes">
+                        <p class="txt-petit">Journée portes ouvertes</p>
+                        <p class="txt-grand">
+                            27/01/2024, <br />
+                            de 10h à 17h
+                        </p>
+                        <p class="en-savoir-plus">EN SAVOIR PLUS</p>
+                    </section>
+                </a>
+            </section>
+        </main>
+    </section>
+</body>
+</html>
