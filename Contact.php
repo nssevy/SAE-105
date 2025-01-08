71d682134085ee28c017d0f8225785d424a06386
filyy03 <toure.fily21@yahoo.com>
2025-01-08 16:47:57 +0100

    radio

--- Contact.php
+++ Contact.php
@@ -26,7 +26,8 @@
         <!-- Vous allez principalement écrire votre code HTML ci-dessous -->
         <main class="conteneur-principal">
             <h1 class="titre-page">CONTACT</h1>
-            <form action="contact.php" method="POST">
+            <h2>Contactez-nous</h2>
+        <form action="contact.php" method="POST">
             <div class="form-group">
                 <label for="email">Adresse Email :</label>
                 <input type="email" id="email" name="email" required>
@@ -40,13 +41,19 @@
                 <input type="text" id="prenom" name="prenom" required>
             </div>
             <div class="form-group">
-                <label for="statut">Statut :</label>
-                <select id="statut" name="statut" required>
-                    <option value="">-- Sélectionnez --</option>
-                    <option value="etudiant">Étudiant</option>
-                    <option value="professionnel">Professionnel</option>
-                    <option value="lyceen">Lycéen</option>
-                </select>
+                <label>Statut :</label>
+                <div>
+                    <input type="radio" id="etudiant" name="statut" value="etudiant" required>
+                    <label for="etudiant">Étudiant</label>
+                </div>
+                <div>
+                    <input type="radio" id="professionnel" name="statut" value="professionnel" required>
+                    <label for="professionnel">Professionnel</label>
+                </div>
+                <div>
+                    <input type="radio" id="lyceen" name="statut" value="lyceen" required>
+                    <label for="lyceen">Lycéen</label>
+                </div>
             </div>
             <div class="form-group">
                 <label for="message">Message :</label>
@@ -54,6 +61,7 @@
             </div>
             <button type="submit">Envoyer</button>
         </form>
+    </div>
             <!-- à continuer ici. Mettre le code respectif de chaque page ici -->
         </main>
     </section>
