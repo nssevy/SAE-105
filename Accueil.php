57eebfe2a478c91afdf4b633f9ed6496ce4f6f1b
hizia <rawat.hizia@gmail.com>
2025-01-09 11:42:47 +0100

    changement titre

--- Accueil.php
+++ Accueil.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>A REMPLACER !!!</title>
+    <title>Accueil</title>
 
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/npm-reset.css">
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/npm-fonts.css">
