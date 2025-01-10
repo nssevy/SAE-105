6d49c02ad87bc04192045d2d76b9ab4a9637ddf0
Yves <sevyns.pro@gmail.com>
2025-01-10 02:04:53 +0100

    Ajout de a-propos.php avec un nouveau contenu et un nouveau style. + Ajout de style-a-propos.css avec des améliorations dela mise en page et du design

--- a-propos.php
+++ a-propos.php
@@ -1,5 +1,5 @@
 <?php
-    $couleur_bulle_classe = "xxx";
+    $couleur_bulle_classe = "vert";
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -8,12 +8,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>A REMPLACER !!!</title>
+    <title>A PROPOS</title>
 
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/npm-reset.css">
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/npm-fonts.css">
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/npm-global.css">
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/npm-header.css">
+    <link rel="stylesheet" href="ressources/style-a-propos.css">
+    
 </head>
 
 <body>
@@ -21,11 +23,31 @@
         <?php require_once('./ressources/includes/header.php'); ?>
 
         <!-- Vous allez principalement écrire votre code HTML ci-dessous -->
-        <main class="conteneur-principal">
-            <h1 class="titre-page">TITRE DE PAGE A REMPLACER !!!</h1>
+        <main class="conteneur-principal">  
+            
+        <div class="menu">
+            <a href="#presentation">Présentation</a>
+            <a href="#sae">SAÉ</a>
+            <a href="#exemples-sae">Exemples de SAÉ</a>
+        </div>
+
+        <div class="text">
+
+                <p>Le BUT métiers du multimédia et de l'internet (MMI) remplace le DUT MMI à partir de la rentrée 2021, auparavant appelé DUT SRC (services et réseaux de communication) jusqu'en mai 2013, qui était lancé à la rentrée universitaire 1994 par les IUT de Vélizy, Marne-la-Vallée, Saint-Raphaël et Verdun. Ce BUT offre une alternative aux anciens diplômes Bac+3, tels que la licence professionnelle en activités et techniques de communication et la licence professionnelle en systèmes informatiques et logiciels.</p>
+
+                <p>Lorsque cette formation était proposée sous la forme d'un Diplôme Universitaire Technologique (DUT), elle se déroulait sur deux années (1 800 heures). En théorie, elle est accessible à tous les bacheliers, quelle que soit leur série. En moyenne, il y a de 30 à 35 heures de cours par semaine. Cette formation se divise en trois grands pôles, auxquels il faut ajouter le projet tutoré (300 heures) et les stages (420 heures).</p>
+
+            <ul>
+                <li>La culture contemporaine et langues étrangères (500 heures)</li>
+                <li>La culture scientifique et technique (700 heures)</li>
+                <li>La culture communicationnelle (600 heures)</li>
+            </ul>
+            
+        </div>
 
             <!-- à continuer ici. Mettre le code respectif de chaque page ici -->
         </main>
     </section>
 </body>
+
 </html>
