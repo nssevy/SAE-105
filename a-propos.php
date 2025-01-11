6089fbf8f150111e6761e340917b434fafece193
Yves <sevyns.pro@gmail.com>
2025-01-12 00:53:34 +0100

    Ajout des images, + modification des espacements entre les paragraphes.

--- a-propos.php
+++ a-propos.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/npm-fonts.css">
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/npm-global.css">
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/npm-header.css">
-    <link rel="stylesheet" href="ressources/style-a-propos.css">
+    <link rel="stylesheet" href="ressources/style-a-propos.css?v=<?php echo time(); ?>">
     
 </head>
 
@@ -25,24 +25,46 @@
         <!-- Vous allez principalement écrire votre code HTML ci-dessous -->
         <main class="conteneur-principal">  
             
-        <div class="menu">
-            <a href="#presentation">Présentation</a>
-            <a href="#sae">SAÉ</a>
-            <a href="#exemples-sae">Exemples de SAÉ</a>
-        </div>
+        <div class="container">
 
-        <div class="text">
+            <div class="menu">
+                <a href="#presentation">Présentation</a>
+                <a href="#sae">SAÉ</a>
+                <a href="#exemples-sae">Exemples de SAÉ</a>
+            </div>
 
+            <div class="text">
                 <p>Le BUT métiers du multimédia et de l'internet (MMI) remplace le DUT MMI à partir de la rentrée 2021, auparavant appelé DUT SRC (services et réseaux de communication) jusqu'en mai 2013, qui était lancé à la rentrée universitaire 1994 par les IUT de Vélizy, Marne-la-Vallée, Saint-Raphaël et Verdun. Ce BUT offre une alternative aux anciens diplômes Bac+3, tels que la licence professionnelle en activités et techniques de communication et la licence professionnelle en systèmes informatiques et logiciels.</p>
 
                 <p>Lorsque cette formation était proposée sous la forme d'un Diplôme Universitaire Technologique (DUT), elle se déroulait sur deux années (1 800 heures). En théorie, elle est accessible à tous les bacheliers, quelle que soit leur série. En moyenne, il y a de 30 à 35 heures de cours par semaine. Cette formation se divise en trois grands pôles, auxquels il faut ajouter le projet tutoré (300 heures) et les stages (420 heures).</p>
 
-            <ul>
-                <li>La culture contemporaine et langues étrangères (500 heures)</li>
-                <li>La culture scientifique et technique (700 heures)</li>
-                <li>La culture communicationnelle (600 heures)</li>
-            </ul>
-            
+                <ul>
+                    <li>La culture contemporaine et langues étrangères (500 heures)</li>
+                    <li>La culture scientifique et technique (700 heures)</li>
+                    <li>La culture communicationnelle (600 heures)</li>
+                </ul>
+
+                <img src="ressources/images/IMG_7106.png"
+                    srcset="ressources/images/IMG_7106@2x.png"
+                    alt="maison">
+                
+                <p>A CY Cergy Paris Université, il est donné la possibilité aux étudiants de passer un semestre au Québec à l'Université du Québec à Chicoutimi (UQAC) dans une formation dont le contenu est proche de celui proposé à l'IUT. Il est également possible d'effectuer ce semestre après avoir été diplômé. <span class="bold"> Attention : les cours sont dispensés en langue française</span>.</p>
+
+            </div>
+
+            <div class="text">
+
+                <h2>Situation d'Apprentissage et d'Évaluation</h2>
+
+                <p>Dans l'optique de préparer au mieux les étudiants à leur future vie professionnelle, les étudiants sont regroupés en agences de communication de 3 à 7 personnes dans des projets appelés "SAÉ" ou Situation d'Apprentissage et d'Évaluation. Ces agences ont pour but d’encourager le travail d'équipe dans un cadre reprenant l'environnement du travail en entreprise.</p>
+                
+                <p>La situation d'apprentissage et d'évaluation ou simplement SAÉ est la situation didactique que privilégie, au Québec, le Ministère de l'Éducation, Enseignement supérieur et Recherche (MEESR) par le biais du Programme de formation de l'école québécoise (PFEQ). Une SAÉ se définit comme un ensemble constitué d'une ou plusieurs tâches à réaliser par l’élève en vue d’atteindre le but fixé. Elle permet : à l'élève, de développer et d’exercer une ou plusieurs compétences disciplinaires et transversales ; à l'enseignant, d’assurer le suivi du développement des compétences dans une perspective d'aide à l'apprentissage. Elle est donc centrée sur l'élève et préconise une approche constructiviste ou socioconstructiviste à l'école.</p>
+
+                <p>Les SAÉ sont une nouveauté des diplômes BUT, elles visent à remplacer les Devoirs Sur Table et les notes à terme, en proposant une approche plus "ingénieure" de la scolarité avec des étudiants qui apprennent à résoudre des problèmes et non plus apprendre par cœur leurs cours.</p>
+
+
+            </div>
+
         </div>
 
             <!-- à continuer ici. Mettre le code respectif de chaque page ici -->
