ab167983b96be8a27fae4e7367acaacddb5f3962
Yves <sevyns.pro@gmail.com>
2025-01-13 22:19:41 +0100

    Page faq faite (à l'aide de figma)

--- Faq.php
+++ Faq.php
@@ -26,7 +26,77 @@
 
         <!-- Vous allez principalement écrire votre code HTML ci-dessous -->
         <main class="conteneur-principal">
-            <h1 class="titre-page">En construction...</h1>
+            <div class="part-1">
+                <div class="question-reponse">
+                    <h1>Questions & Réponses</h1>
+                    <p>Sur notre site, nous mettons à votre disposition un espace interactif où nos visiteurs peuvent poser des questions et partager leurs réponses. Ce service permet d’obtenir des informations précises et détaillées grâce aux contributions de notre communauté.</p>
+                </div>
+
+                <div class="choisis-theme">
+                    <h2>Choisissez un thème :</h2>
+                    <div class="lien">
+                        <p>index</p>
+                        <p>index</p>
+                        <p>index</p>
+                        <p>index</p>
+                        <p>index</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="part-2">
+
+                <div class="component">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1007" height="2" viewBox="0 0 1007 2" fill="none">
+                    <path d="M0 1H1007" stroke="#BBBBBB"stroke-width="1.5"/>
+                    </svg>
+                    <div class="frame">
+                        <h2>Index</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  </p>
+                    </div>
+                </div>
+
+                <div class="component">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1007" height="2" viewBox="0 0 1007 2" fill="none">
+                    <path d="M0 1H1007" stroke="#BBBBBB"stroke-width="1.5"/>
+                    </svg>
+                    <div class="frame">
+                        <h2>Index</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  </p>
+                    </div>
+                </div>
+
+                <div class="component">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1007" height="2" viewBox="0 0 1007 2" fill="none">
+                    <path d="M0 1H1007" stroke="#BBBBBB"stroke-width="1.5"/>
+                    </svg>
+                    <div class="frame">
+                        <h2>Index</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  </p>
+                    </div>
+                </div>
+
+                <div class="component">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1007" height="2" viewBox="0 0 1007 2" fill="none">
+                    <path d="M0 1H1007" stroke="#BBBBBB"stroke-width="1.5"/>
+                    </svg>
+                    <div class="frame">
+                        <h2>Index</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  </p>
+                    </div>
+                </div>
+
+                <div class="component">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1007" height="2" viewBox="0 0 1007 2" fill="none">
+                    <path d="M0 1H1007" stroke="#BBBBBB"stroke-width="1.5"/>
+                    </svg>
+                    <div class="frame">
+                        <h2>Index</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  </p>
+                    </div>
+                </div>
+
+            </div>
 
             <!-- à continuer ici. Mettre le code respectif de chaque page ici -->
         </main>
