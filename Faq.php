23e6358d11cd3e5a6f9db5abd3ddcf6589480068
Yves <sevyns.pro@gmail.com>
2025-01-13 21:00:47 +0100

    Ajout de la page FAQ plus les llinks etc, modification du footer sur le copyright

---
+++ Faq.php
@@ -0,0 +1,40 @@
+<?php
+    $couleur_bulle_classe= "marron";
+    $couleur_bordure_classe="marronfonce";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FAQ</title>
+
+    <link rel="stylesheet" href="ressources/css/ne-pas-modifier/npm-reset.css">
+    <link rel="stylesheet" href="ressources/css/ne-pas-modifier/npm-fonts.css">
+    <link rel="stylesheet" href="ressources/css/ne-pas-modifier/npm-global.css">
+    <link rel="stylesheet" href="ressources/css/ne-pas-modifier/npm-header.css">
+    <link rel="stylesheet" href="ressources/css-élève/style-faq.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="ressources/css-élève/style-footer.css">
+
+</head>
+
+<body>
+    <section class="conteneur-1280">
+        <?php require_once('./ressources/includes/header.php'); ?>
+
+        <!-- Vous allez principalement écrire votre code HTML ci-dessous -->
+        <main class="conteneur-principal">
+            <h1 class="titre-page">En construction...</h1>
+
+            <!-- à continuer ici. Mettre le code respectif de chaque page ici -->
+        </main>
+    </section>
+</body>
+
+<footer>
+    <?php require_once('./ressources/includes/footer.php'); ?>
+</footer>
+
+</html>
