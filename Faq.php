f918211a499a419e7118785088c6cccf2a975693
filyy03 <toure.fily21@yahoo.com>
2025-01-18 16:09:45 +0100

    | BUT MMI

--- Faq.php
+++ Faq.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FAQ</title>
+    <title>FAQ | BUT MMI</title>
 
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/npm-reset.css">
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/npm-fonts.css">
--- a-propos.php
+++ a-propos.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>A PROPOS</title>
+    <title>A PROPOS | BUT MMI</title>
 
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/npm-reset.css">
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/npm-fonts.css">
